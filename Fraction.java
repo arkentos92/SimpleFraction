b79bd5f86aa67c443baec01d5505fb3dc51ef100
Ewan TREBERN <ewan.trebern@ens.uvsq.fr>
2023-10-06 05:26:01 +0200

    added method tostring() to class Fraction

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,8 @@
 public class Fraction {
+  static String tostring() {
+    return "Je suis une fraction.";
+  }
   public static void main(String[] args) {
-    
+    System.out.println(tostring());
   }
 }
